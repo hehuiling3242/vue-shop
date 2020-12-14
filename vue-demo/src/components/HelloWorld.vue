ccf28838676b890de9af1bc5bc95ddbdbfb3860e
hehuiling <hehuiling@zhejian.com>
2020-12-14 11:07:30 +0800

    1214新建登录

---
+++ vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <ul>
+      <items v-for="(model, index) in list" :model="model" :key="index"></items>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Items from "./items";
+
+export default {
+  components: {
+    Items
+  },
+  data() {
+    return {
+      list: [
+        {
+          id: "1",
+          menuName: "haha",
+          childTree: [
+            {
+              menuName: "一",
+              childTree: [
+                {
+                  menuName: "1",
+                  childTree: [
+                    {
+                      menuName: "2",
+                      childTree: [
+                        {
+                          menuName: "3",
+                          childTree: [{ menuName: "3" }]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              menuName: "二"
+            }
+          ]
+        },
+        {
+          id: "2",
+          menuName: "heihei"
+        },
+        {
+          id: "3",
+          menuName: "enen"
+        },
+        {
+          id: "4",
+          menuName: "hehe",
+          childTree: [
+            {
+              menuName: "一",
+              childTree: [{ menuName: "1" }]
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ vue-demo/src/components/items.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <span @click="toggle">
+      {{ model.menuName }}
+    </span>
+    <ul v-if="isFolder" v-show="open">
+      <items
+        v-for="(item, index) in model.childTree"
+        :model="item"
+        :key="index"
+      ></items>
+    </ul>
+  </li>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "items",
+  props: ["model"],
+  data() {
+    return {
+      open: false
+    };
+  },
+  computed: {
+    isFolder() {
+      return this.model.childTree && this.model.childTree.length;
+    }
+  },
+  methods: {
+    toggle() {
+      if (this.isFolder) {
+        this.open = !this.open;
+      }
+    }
+  }
+};
+</script>
